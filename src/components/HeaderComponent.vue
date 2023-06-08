9f757ab33579de722eecffc6fd4f7fde40ab4b01
jurmaev <smartemii03@gmail.com>
2023-06-08 17:39:38 +0500

    fix nav

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -45,10 +45,6 @@ export default {
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <form class="d-flex mx-auto">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <template v-if="userStore.username">
                         <li class="nav-item">
